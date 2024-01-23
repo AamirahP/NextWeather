273f9565aec17d93f8e3f377f1f73172fb488c0a
Aamirah <aamirahpatel@hotmail.co.uk>
2024-01-23 11:53:24 +0000

    trying to dynamic route

---
+++ src/app/main/page.tsx
@@ -0,0 +1,125 @@
+"use client";
+import { useEffect, useState } from "react";
+import SearchBar from "@/Components/SearchBar";
+import WeatherData from "@/Components/WeatherData";
+import Wind from "@/Components/Wind";
+import Humidity from "@/Components/Humidity";
+import WeatherImage from "@/Components/WeatherImage";
+import WeatherDescription from "@/Components/WeatherDescription";
+import WeatherInfoTable from "@/Components/WeatherInfoTable";
+import Link from "next/link";
+
+interface WeatherData {
+  // params: {
+  main: {
+    temp: number;
+    feels_like: number;
+    humidity: number;
+    temp_max: number;
+    temp_min: number;
+  };
+  name: string;
+  wind: {
+    speed: number;
+  };
+  weather: {
+    icon: string;
+    description: string;
+  }[];
+  sys: {
+    sunrise: number;
+    sunset: number;
+  };
+}
+// }
+
+function App() {
+  const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
+  const [city, setCity] = useState("London");
+
+  const search = async (searchCity?: string) => {
+    try {
+      const apiUrl = `/api/page?city=${encodeURIComponent(searchCity || city)}`;
+      console.log("API URL:", apiUrl);
+
+      const response = await fetch(apiUrl);
+
+      console.log("Response status:", response.status);
+
+      if (!response.ok) {
+        const errorMessage = await response.text();
+        console.error(`Failed to fetch data. Server response: ${errorMessage}`);
+        return;
+      }
+
+      const data = await response.json();
+      console.log("Response from OpenWeatherMap:", data);
+      localStorage.setItem("weatherData", JSON.stringify(data));
+
+      setWeatherData(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    search();
+  }, []);
+
+  return (
+    <>
+      <div className="Container">
+        <SearchBar
+          search={(searchCity?: string) => search(searchCity)}
+          city={city}
+          setCity={setCity}
+        />
+
+        {weatherData &&
+          weatherData.main &&
+          weatherData.wind &&
+          weatherData.sys && (
+            <>
+              <WeatherData
+                temperature={Math.round(weatherData.main.temp)}
+                location={weatherData.name}
+              />
+              <Link
+                className="info"
+                href={`/main/data?city=${encodeURIComponent(
+                  weatherData?.name || city
+                )}`}
+              >
+                Click for more info {">"} {">"} {">"}
+              </Link>
+
+              <div className="weather-image">
+                <WeatherImage iconCode={weatherData?.weather[0].icon} />
+              </div>
+
+              <div className="description">
+                <WeatherDescription
+                  description={weatherData.weather[0].description}
+                />
+              </div>
+
+              <div className="elements">
+                <Wind windSpeed={weatherData.wind.speed} />
+                <Humidity humidity={weatherData.main.humidity} />
+              </div>
+            </>
+          )}
+      </div>
+      <br></br>
+      <WeatherInfoTable
+        sunrise={weatherData?.sys.sunrise || 0}
+        sunset={weatherData?.sys.sunset || 0}
+        minTemp={weatherData?.main.temp_min || 0}
+        maxTemp={weatherData?.main.temp_max || 0}
+        feelsLike={weatherData?.main.feels_like || 0}
+      />
+    </>
+  );
+}
+
+export default App;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,136 +1,27 @@
-"use client";
-import { useEffect, useState } from "react";
-import SearchBar from "../Components/SearchBar";
-import WeatherData from "../Components/WeatherData";
-import Wind from "../Components/Wind";
-import Humidity from "../Components/Humidity";
-import WeatherImage from "../Components/WeatherImage";
-import WeatherDescription from "../Components/WeatherDescription";
-import WeatherInfoTable from "@/Components/WeatherInfoTable";
+import Link from "next/link";
+import React from "react";
 
-interface infoProps {
-  params: {
-    main: {
-      feels_like: number;
-      temp_max: number;
-      temp_min: number;
-    };
-    sys: {
-      sunrise: number;
-      sunset: number;
-    };
-  };
-}
-
-interface WeatherData {
-  main: {
-    temp: number;
-    feels_like: number;
-    humidity: number;
-    temp_max: number;
-    temp_min: number;
-  };
-  name: string;
-  wind: {
-    speed: number;
-  };
-  weather: {
-    icon: string;
-    description: string;
-  }[];
-  sys: {
-    sunrise: number;
-    sunset: number;
-  };
-}
-
-function App() {
-  const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
-  const [city, setCity] = useState("London");
-
-  const search = async (searchCity?: string) => {
-    try {
-      const apiUrl = `/api/page?city=${encodeURIComponent(searchCity || city)}`;
-      console.log("API URL:", apiUrl);
-  
-      const response = await fetch(apiUrl);
-  
-      console.log("Response status:", response.status);
-  
-      if (!response.ok) {
-        const errorMessage = await response.text();
-        console.error(`Failed to fetch data. Server response: ${errorMessage}`);
-        return;
-      }
-  
-      const data = await response.json();
-      console.log("Response from OpenWeatherMap:", data);
-      localStorage.setItem("weatherData", JSON.stringify(data));
-  
-      setWeatherData(data);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
-
-  useEffect(() => {
-    search();
-  }, []);
-
+const Welcome = () => {
   return (
     <>
       <div className="Container">
-        <SearchBar
-          search={(searchCity?: string) => search(searchCity)}
-          city={city}
-          setCity={setCity}
-        />
-
-        {weatherData &&
-          weatherData.main &&
-          weatherData.wind &&
-          weatherData.sys && (
-            <>
-              <WeatherData
-                temperature={Math.round(weatherData.main.temp)}
-                location={weatherData.name}
-              />
-              <p className="info">
-                <a href="http://localhost:3000/main">
-                  {" "}
-                  Click for more info {">"}
-                  {">"}
-                  {">"}
-                </a>
-              </p>
-
-              <div className="weather-image">
-                <WeatherImage iconCode={weatherData?.weather[0].icon} />
-              </div>
-
-              <div className="description">
-                <WeatherDescription
-                  description={weatherData.weather[0].description}
-                />
-              </div>
-
-              <div className="elements">
-                <Wind windSpeed={weatherData.wind.speed} />
-                <Humidity humidity={weatherData.main.humidity} />
-              </div>
-            </>
-          )}
+        <p className="Paragraph">
+          <span className="SpecialText">
+            What's worse than rain? Forgetting your umbrella.. :(
+          </span>
+          <br />
+          Welcome to my WeatherApp Project. I'm an outdoorsy person, but I
+          always seem to dress wrong for the weather. Whether I'm wrapped up too
+          much for hikes or caught without my raincoat in a storm. This app is
+          my attempt to challenge myself technically while creating something
+          genuinely useful. Join me in staying weather-ready!
+        </p>
+        <div className="LinkContainer">
+          <Link href="/main">Explore the WeatherApp</Link>
+        </div>
       </div>
-      <br></br>
-      <WeatherInfoTable
-        sunrise={weatherData?.sys.sunrise || 0}
-        sunset={weatherData?.sys.sunset || 0}
-        minTemp={weatherData?.main.temp_min || 0}
-        maxTemp={weatherData?.main.temp_max || 0}
-        feelsLike={weatherData?.main.feels_like || 0}
-      />
     </>
   );
-}
+};
 
-export default App;
+export default Welcome;
