14024642608e35ec80c3f6d499ccf3b982f32020
Aamirah <aamirahpatel@hotmail.co.uk>
2024-04-18 23:17:22 +0100

    report coverage stuff

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,18 @@
-import type {Config} from 'jest'
-import nextJest from 'next/jest.js'
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
 
 const createJestConfig = nextJest({
   dir: './',
-})
-
+});
 
 const config: Config = {
+  ...createJestConfig,
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
-}
+  reporters: [
+    'default',
+    ['jest-json-reporter', { outputPath: 'test-results/test-report.json' }]
+  ],
+};
 
-export default createJestConfig(config)
+export default config;
