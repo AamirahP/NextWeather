0acd52e1043b27baf985be9fa74e091f62c491cf
Aamirah <aamirahpatel@hotmail.co.uk>
2024-04-18 23:31:20 +0100

    dfghj

--- jest.config.ts
+++ jest.config.ts
@@ -1,18 +1,15 @@
-import type { Config } from 'jest';
-import nextJest from 'next/jest.js';
+import type {Config} from 'jest'
+import nextJest from 'next/jest.js'
 
 const createJestConfig = nextJest({
   dir: './',
-});
+})
+
 
 const config: Config = {
-  ...createJestConfig,
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
-  reporters: [
-    'default',
-    ['jest-json-reporter', { outputPath: 'test-results/test-report.json' }]
-  ],
-};
+}
 
-export default config;
+
+export default createJestConfig(config)
